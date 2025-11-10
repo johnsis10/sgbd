f5cf818cddbe835fb44e59b05af4582c530e25e3
mar080685 <mar.lenizag@gmail.com>
2025-11-10 17:21:28 -0400

    creamos la clase rol

--- partelogica/src/app.module.ts
+++ partelogica/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
 import { RolModule } from './rol/rol.module';
+import { RolModule } from './rol/rol.module';
 
 @Module({
   imports: [UsuarioModule, RolModule],
