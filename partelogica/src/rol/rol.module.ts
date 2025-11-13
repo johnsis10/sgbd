4c7ebb521a4d2c5d485b2d7afe1a986c0d055287
Juan Albaro Laime Choque <jlaimechoque@gmail.com>
2025-11-13 12:53:27 -0400

    prueba

--- partelogica/src/rol/rol.module.ts
+++ partelogica/src/rol/rol.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Rol } from './rol.entity';
 import { RolService } from './rol.service';
 import { RolController } from './rol.controller';
 
-
 @Module({
+  imports: [TypeOrmModule.forFeature([Rol])],
   providers: [RolService],
-  controllers: [RolController]
+  controllers: [RolController],
+  exports: [TypeOrmModule], 
 })
 export class RolModule {}
--- partelogica/src/usuario/usuario.module.ts
+++ partelogica/src/usuario/usuario.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Usuario } from './usuario.entity';
+import { Rol } from '../rol/rol.entity';
 import { UsuarioService } from './usuario.service';
+import { UsuarioController } from './usuario.controller';
 
-@Module({
-  imports: [TypeOrmModule.forFeature([Usuario])],
+@Module({         
+  imports: [TypeOrmModule.forFeature([Usuario, Rol])], 
   providers: [UsuarioService],
+  controllers: [UsuarioController],
   exports: [UsuarioService],
 })
 export class UsuarioModule {}
