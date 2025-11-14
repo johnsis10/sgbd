3d9b9ec74da27ff52e19a12c633185e729cb151c
Juan Albaro Laime Choque <jlaimechoque@gmail.com>
2025-11-14 02:31:11 -0400

    prueba2

--- partelogica/src/autentificacion/autentificacion.controller.ts
+++ partelogica/src/autentificacion/autentificacion.controller.ts
@@ -1,15 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AutentificacionService } from './autentificacion.service';
+import { AccesoDto } from './dto/acceso.dto';
 
 @Controller('api/v1/autentificacion')
 export class AutentificacionController {
-  constructor(
-        private autentificacionService: AutentificacionService
-    ) {}
+  constructor(private readonly autentificacionService: AutentificacionService) {}
 
-    @Post('acceso')
-    async acceso(@Body() datos: {eusuario: string, eclave: string}) {
-        const usuario = await this.autentificacionService.validarUsuario(datos.eusuario, datos.eclave);
-        return this.autentificacionService.acceso(usuario);
-    }
+  @Post('acceso')
+  async acceso(@Body() datos: AccesoDto) {
+    const usuario = await this.autentificacionService.validarUsuario(
+      datos.correo,
+      datos.contrasena,
+    );
+    return this.autentificacionService.acceso(usuario);
+  }
 }
--- partelogica/src/autentificacion/dto/acceso.dto.ts
+++ partelogica/src/autentificacion/dto/acceso.dto.ts
@@ -2,8 +2,8 @@ import { IsEmail, IsString } from 'class-validator';
 
 export class AccesoDto {
   @IsEmail()
-  eusuario: string;
+  correo: string;
 
   @IsString()
-  eclave: string;
+  contrasena: string;
 }
